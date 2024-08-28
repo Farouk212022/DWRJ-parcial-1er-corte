ac114cbea7a8ca4c58c15d11353ea358b81380b3
Farouk212022 <faroukabdala21@gmail.com>
2024-08-27 23:19:14 -0500

    Sube solución al ejercicio propuesto como Parcial de 1er Corte

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
-import RickAndMortyService from './service';
-
+import RickAndMortyService from "./service.js";
 
 // acá deberás crear una instancia del servicio RickAndMortyService
-// const service = new RickAndMortyService();
+const service = new RickAndMortyService();
 
 // esta función debe encargarse de obtener el elemento contenedor
 // y agregar los personajes obtenidos por el API, deberás llamar tu función getAllCharacters
 // iterar el arreglo de personajes y llamar a la función createCharacterCard para agregar cada personaje
 // a el contenedor puedes usar la propiedad innerHTML para esto
 
-
-function createCharacterList() {
-    // llamar primero createCharacterCard(character);
-    // llamar segundo addCharacterListeners(character);
+async function createCharacterList() {
+  try {
+    var characterList = await service.getAllCharacters();
+    const characterListDiv = document.querySelector(".character-list");
+    for (var i = 0; i < characterList.length; i++) {
+      let character = characterList[i];
+      const characterCard = createCharacterCard(character);
+      characterListDiv.insertAdjacentHTML('beforeend', characterCard);
+      console.log(i,characterListDiv)
+      addCharacterListeners(character);
+    }
+  } catch (e) {
+    document.getElementById(
+      "app"
+    ).innerHTML = `<div>Se ha generado el siguiente error al llamar al crear las cartas: ${e}</div>`;
+  }
+  // llamar primero createCharacterCard(character);
+  // llamar segundo addCharacterListeners(character);
 }
 
 // esta función debe devolver la estructura html en string de tu personaje ejemplo
@@ -24,16 +37,52 @@ function createCharacterList() {
 
 // deberás usar los elementos correctos de HTML para poder visualizar el personaje
 
-
-function createCharacterCard(character) {}
+function createCharacterCard(character) {
+  const statusCase = character["status"].toLowerCase();
+  return `<div class="character-card">
+                <img src="${character["image"]}" class="character-image"/>
+                <div class="character-information">
+                  <div class="character-description">
+                    <span class="character-name">${character["name"]}</span>
+                    <div class="characteristic">
+                      <img class="characteristic-image" id="${statusCase}" src="./assets/live.svg">
+                      <span>${character["status"]}</span>
+                    </div>
+                    <div class="characteristic">
+                      <img class="characteristic-image" src="./assets/race.svg">
+                      <span>${character["species"]}</span>
+                    </div>
+                    <div class="characteristic">
+                      <img class="characteristic-image" src="./assets/planet.svg">
+                      <span>${character["firstSeen"]["name"]}</span>
+                    </div>
+                  </div>
+                  <img class= "heart-image" src="./assets/heart.svg" id="heart-button-${character["name"]}">
+                </div>
+            </div>`;
+}
 
 // esta función deberá obtener todos los personajes y deberá agregarles un evento de click en el icono de corazon
-// cuando se haga click al icono de corazon aparecer una alerta con un mensaje 
+// cuando se haga click al icono de corazon aparecer una alerta con un mensaje
 // que diga Hola soy (nombre personaje), recuerda que puedes obtener
 // el elemento target de un evento y así obtener sus propiedades
 
-function addCharacterListeners(character) {}
-
+function addCharacterListeners(character) {
+  try {
+    let idListener= "heart-button-"+`${character["name"]}`
+    let heartButton = document.getElementById(idListener);
+    console.log(heartButton);
+    heartButton.addEventListener("click", function (event) {
+      alert(
+        `Hola soy ${character["name"]} y este es el trabajo de Carlos Farouk Abdalá`
+      );
+    });
+  } catch (e) {
+    document.getElementById(
+      "app"
+    ).innerHTML = `<div>Se ha generado el siguiente error al llamar al crear el Listener: ${e}</div>`;
+  }
+}
 
 // por último se llama la función y se renderiza la vista
 createCharacterList();
--- src/service.js
+++ src/service.js
@@ -38,46 +38,93 @@
 //     ]
 //   }
 
-
 class RickAndMortyService {
-    // el constructor debe inicializar una variable con la url de acceso base al API
+  // el constructor debe inicializar una variable con la url de acceso base al API
 
-	constructor() {}
+  constructor() {
+    this.apiUrl = "https://rickandmortyapi.com/api/character";
+  }
 
-    
-    // este método deberá llamar al servicio y obtener los personajes
-    // deberá devolver un objeto de la siguiente manera
+  // este método deberá llamar al servicio y obtener los personajes
+  // deberá devolver un objeto de la siguiente manera
 
-    // {
-    //     "name": "Rick Sanchez",
-    //     "status": "Alive",
-    //     "species": "Human",
-    //     "firstSeen": "Earth",
-    //     "location": "Earth",
-    //     "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-    //     "student": "aqui va el nombre del estudiante",
-    //     "code": "aqui va el codigo del estudiante"
-    // }
+  // {
+  //     "name": "Rick Sanchez",
+  //     "status": "Alive",
+  //     "species": "Human",
+  //     "firstSeen": "Earth",
+  //     "location": "Earth",
+  //     "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+  //     "student": "aqui va el nombre del estudiante",
+  //     "code": "aqui va el codigo del estudiante"
+  // }
 
-    // deberá realizar el respectivo manejo de error en caso de errores al llamar el API
-    // se recomienda usar el api fetch para obtener los datos como en el siguiente ejemplo  
+  // deberá realizar el respectivo manejo de error en caso de errores al llamar el API
+  // se recomienda usar el api fetch para obtener los datos como en el siguiente ejemplo
 
-    // ejemplo con promesas
+  // ejemplo con promesas
 
-    // fetch('miurl')
-    //  .then((respuesta) => respuesta.json())
-    //  .then((mispersonajes) => {
-    //     //aqui dentro mi logica
-    //  })
+  // fetch('miurl')
+  //  .then((respuesta) => respuesta.json())
+  //  .then((mispersonajes) => {
+  //     //aqui dentro mi logica
+  //  })
 
-    //  ejemplo con async/await
+  //  ejemplo con async/await
 
-    //  const response = await fetch('miurl');
-    //  const mispersonajes = await response.json();
+  //  const response = await fetch('miurl');
+  //  const mispersonajes = await response.json();
 
-	getAllCharacters() {
-        // aqui va tu llamado al API usando fetch puedes usar promesas o asycn/await
-	}
+  async getAllCharacters() {
+    // aqui va tu llamado al API usando fetch puedes usar promesas o asycn/await
+    try {
+      var finalCharacterList = [];
+      const apiResponse = await fetch(this.apiUrl);
+      const charactersResponse = await apiResponse.json();
+      const charactersList = charactersResponse["results"];
+      for (var i = 0; i < 8; i++) {
+        var character = charactersList[i];
+        var characterObject = new Character(
+          character["name"],
+          character["status"],
+          character["species"],
+          character["origin"],
+          character["location"],
+          character["image"],
+          "Farouk Abdala",
+          242237
+        );
+        finalCharacterList.push(characterObject);
+      }
+      return finalCharacterList;
+    } catch (e) {
+      document.getElementById(
+        "app"
+      ).innerHTML = `<div>Se ha generado el siguiente error al llamar al API: ${e}</div>`;
+    }
+  }
+}
+
+class Character {
+  constructor(
+    name,
+    status,
+    species,
+    firstSeen,
+    location,
+    image,
+    student,
+    code
+  ) {
+    this.name = name;
+    this.status = status;
+    this.species = species;
+    this.firstSeen = firstSeen;
+    this.location = location;
+    this.image = image;
+    this.student = student;
+    this.code = code;
+  }
 }
 
 export default RickAndMortyService;
